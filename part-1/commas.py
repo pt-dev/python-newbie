c944c9766baa819d39a53e8a7b11fd94a680777f
pt-dev <david.slonosky@gmail.com>
2019-09-20 21:55:03 -0400

    Joining strings

---
+++ part-1/commas.py
@@ -0,0 +1,6 @@
+foo = "Lutefisk"
+bar = "Norway"
+cho = "Iceland"
+
+print(",".join(['a','b','ddd','smooks']))
+print(",".join([foo,bar,cho]))
---
+++ part-1/joinstr.py
@@ -0,0 +1,6 @@
+balloon = "Sammy has a balloon"
+foo = " ".join(balloon)
+print(balloon)
+print(foo)
+
+print("".join(reversed(balloon)))
