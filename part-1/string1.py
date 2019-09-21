1eaa2d6ecfc2fd6eee8df4160bd013e5b0a34eb0
pt-dev <david.slonosky@gmail.com>
2019-09-20 21:43:18 -0400

    Testing string functions part 1

---
+++ part-1/string1.py
@@ -0,0 +1,8 @@
+ss = "Sammy Shark"
+print(ss.upper())
+print(ss.lower())
+print("Is it all alphanum? ")
+print(str.isalnum(ss))
+print("What about all alpha?")
+print(str.isalnum(ss))
+print(ss)
---
+++ part-1/testemstrings.py
@@ -0,0 +1,20 @@
+movie = "2001: A SAMMY ODYSSEY"
+book = "A Thousand Splendid Sharks"
+poem = "sammy lived in a pretty how town"
+nospaces = "sammywasaFINEfella"
+
+print(movie.islower())
+print(movie.isupper())
+
+print(book.istitle())
+print(book.isupper())
+
+print(poem.istitle())
+print(poem.islower())
+
+
+print("Is it all alphanum? ")
+print(str.isalnum(nospaces))
+print("What about all alpha?")
+print(str.isalnum(nospaces))
+print(nospaces)
