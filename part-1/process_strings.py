1c3d4e8eca76844f31f2d4d6b190f7db6a1dafc7
pt-dev <david.slonosky@gmail.com>
2020-03-03 18:37:33 -0500

    Update process_strings.py
    Improve English in prompts

--- part-1/process_strings.py
+++ part-1/process_strings.py
@@ -1,10 +1,10 @@
 max_len = 79
 str_plier = 2
-foo = "Cherries, peaches, avocados. Oh my!"*str_plier
+foo = "Oh oh oh oh you don't know, Joe."*str_plier
 curr_len = len(foo)
 
 if curr_len > max_len:
     print("Danger, Will Robinson!!!")
-    print("Length is {} ".format(curr_len-max_len)+"characters too long!")
+    print("Your sentiment is {} ".format(curr_len-max_len)+"characters too long!")
 else:
     print("What a lovely sentiment!\n"+ foo+"\nIn only {} characters!".format(curr_len))
