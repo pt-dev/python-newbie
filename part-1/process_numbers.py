3c0043af6071facb73177214670718e45bce8575
pt-dev <david.slonosky@gmail.com>
2020-03-03 19:06:52 -0500

    Create process_numbers.py

---
+++ part-1/process_numbers.py
@@ -0,0 +1,10 @@
+my_int = 22
+my_float = 64.21
+my_str_num = "38.24"
+print("Int my Float! "+str(int(my_float)))
+print("Float my int! "+str(float(my_int)))
+print("Num my string! "+str(float(my_str_num)))
+#add
+foo = float(my_str_num)
+foo = foo+my_float
+print(foo)
